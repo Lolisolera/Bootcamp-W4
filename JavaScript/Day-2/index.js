9629b9a26c4ba566391e7757ff6c11170f1a6f18
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-10-10 08:44:15 +0100

    syntax correction on JS files

--- JavaScript/Day-2/index.js
+++ JavaScript/Day-2/index.js
@@ -3,17 +3,18 @@
 //TASK -MONDAY 9/10/2023
 //1: Create a variable that stores your name and log a string to the console that personally greets you using template literals.
 
-const firstName = ("Lola");
-const lastName = ("Marquez");
+const firstName = "Lola";
+const lastName = "Marquez";
 console.log(`Hello,  ${firstName} ${lastName}!`);
+alert(`Hello,  ${firstName} ${lastName}!`);
 
 //2: Create variables storing your name, your favourite colour and your favourite film or tv series. Using template literals I want you to log a sentence including these values to the console.
-let favouriteColor = ("Red");
-let favouriteFilm = ("Lost in Translation");
-console.log(`My name is ${firstName}. ${favouriteColor} is my favorite color and ${favouriteFilm} is my favorite film.`);
+let favouriteColor = "Red";
+let favouriteFilm = "Lost in Translation";
+console.log(`My name is ${firstName}. ${favouriteColor} is my favourite color and ${favouriteFilm} is my favourite film.`);
 
 //3: Create variables storing what you ate for breakfast, lunch and dinner yesterday and log them to the console. Without redeclaring new variables I would like you to assign a new value of what you ate today to the variables, and again log them to the console.
-let breakfast = ("coffee and cereal");
-let lunch = ("ham and cheese sandwich");
-let dinner = ("spagetti bolognaise with a side salad");
+let breakfast = "coffee and cereal";
+let lunch = "ham and cheese sandwich";
+let dinner = "spagetti bolognaise with a side salad";
 console.log(`Yesterday I had ${breakfast} for breakfast, ${lunch} for lunch, and ${dinner} for dinner.`);
