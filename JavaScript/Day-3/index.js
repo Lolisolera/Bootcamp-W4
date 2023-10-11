4164a9150dc4df4014ec17f93241118de47b3b8b
Lola Marquez <122627993+Lolisolera@users.noreply.github.com>
2023-10-11 11:48:03 +0100

    Update index.js

--- JavaScript/Day-3/index.js
+++ JavaScript/Day-3/index.js
@@ -1,1 +1,165 @@
-alert("Hola");
+//ARRAYS
+/*
+let coffeeOrder = [
+    "James - Americano",
+    "Hanna - Latte",
+    "Alex - Capucino",
+]
+
+//Log the entire Array in the console.
+console.log(coffeeOrder);
+
+//In the contex of an Array, the lengh property represents the number of items.
+
+console.log(coffeeOrder.length);
+
+
+//Log individual items to the console.
+console.log(coffeeOrder[0]);
+console.log(coffeeOrder[1]);
+console.log(coffeeOrder[2]);
+
+//Add items to an Array via the idex:
+
+coffeeOrder[1] = "Hanna - Tea";
+console.log(coffeeOrder[1]);
+
+coffeeOrder[3] = "Christian - water";
+console.log(coffeeOrder);
+
+
+//Array Methods
+
+//.pop method - Removes the last item from the array
+
+coffeeOrder.pop();
+console.log(coffeeOrder);
+
+
+//.push Method add an item to the end of an array
+
+coffeeOrder.push("Christian - Apple Juice");
+console.log(coffeeOrder);
+
+ console.table
+
+console.table(coffeeOrder);
+
+//LOOPS
+let colors = [
+    "Blue",
+    "Red",
+    "Pink",
+    "Green",
+    "Purple",
+    "Yellow",
+
+]
+console.log(colors);
+console.log(colors[0]);
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
+console.log(colors[4]);
+console.log(colors[5]);
+
+
+//Increment operator = ++ Where the ++ is located it'll determine the value
+//If you put it befoer it'll increment the value
+//If you put it after it'll keep the original value
+let a = 1;
+console.log(a);
+
+let b = a++
+console.log(b);
+console.log(a);
+
+//For Loops
+//for(let i initiasatonExpression or index or counter)
+
+/for (let i = 0; i< colors.length; i++){
+console.log(`index value is currently ${1}`);
+
+}
+
+for (let i = 0; 1 < colors.length; i++) {
+    console.log(colors[i]);
+}
+
+
+//WHILE LOOPS
+// let number =0;
+//while(nmber <10){
+//console.log(number);
+//number++
+//}
+
+
+
+let cards = ["Diamond", "Heart", "Club", "Spade"];
+
+let currentCard = "";
+
+
+while (currentCard !== "Spade") {
+
+    currentCard = cards[Math.floor(Math.random() * 4)];
+
+    console.log(currentCard);
+
+}
+
+
+console.log("!!! GAME OVER !!!");
+
+
+
+
+
+//while LOOP
+
+let num = 1;
+
+do {
+
+    console.Log(num);
+    while(num<1);
+
+}
+
+
+
+//BREAK keyword in loop
+
+for (let i =1; 1<=5; 1++){
+    if (i ===3){
+        break
+    }
+    console.log(i);
+}
+
+//For loop -Array
+
+
+
+*/
+
+
+
+//Tasks: 
+
+//1: Create an array of your favourite Films / TV shows, up to 5 items. Use an array method to add 2 more items to your array. Use a loop to cycle through the array and log each item to the console.
+
+//2: Write a loop to generate 10 random numbers between 1-100 and log them to the console.
+
+//3: Write a loop that counts backwards from 20-0, logging each number to the console.
+
+//4: Generate 5 random numbers between 1-50. For each number generated, check if the number is divisible by 5 or not. Log whether it is divisible or not to the console.
+
+
+
+
+
+
+
+
